c20efd6ef38108f4ca2b68e9a5088d6e1b07a67c
serenity smith <serenitysmith24@gmail.com>
2023-02-07 00:13:07 -0500

    push

---
+++ app.js
@@ -0,0 +1,52 @@
+
+//Loop through the following array and count how many "computers" there are. Log the final count:
+var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+var count = 0;
+for( var i = 0; i < officeItems.length; i++){
+if(officeItems[i] == "computer")
+count++;
+}
+console.log(count)
+// if (officeItems [i]  === 'computer'){
+//     console.log ([i] + number)
+// } this just logged each number of the computers
+
+
+
+
+//Loop through the following array and log to the console "old enough" if they are 18 or older, and "not old enough" if they aren't 18.
+
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+  for( var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
+    if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18) {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " old enough"); 
+    }else {
+        console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " not old enough")
+    }
+    
+    
+
+  }
